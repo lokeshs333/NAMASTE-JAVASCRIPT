76a0fbdfb16152a2754a26f7cb60265ad9b25914
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-05-17 15:02:46 +0530

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,16 @@
+// block concepcept in javascript.....
+var a = 1;
+let b = 10;
+const c = 20;
+{
+    a = 2;
+    let b = 11;
+    const c = 21;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log("lokesh pandey ");
+}
+console.log(a);
+console.log(b);
+console.log(c);
